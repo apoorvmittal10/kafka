f43a01057f0f9a2651d9f5a567850691393bb490
Apoorv Mittal <amittal@confluent.io>
2023-10-20 22:15:24 +0100

    KAFKA-15618: Adding KafkaMetricsCollector and supporting telemetry classes (KIP-714)

--- clients/src/main/java/org/apache/kafka/common/telemetry/internals/SinglePointMetric.java
+++ clients/src/main/java/org/apache/kafka/common/telemetry/internals/SinglePointMetric.java
@@ -16,6 +16,15 @@
  */
 package org.apache.kafka.common.telemetry.internals;
 
+import io.opentelemetry.proto.common.v1.AnyValue;
+import io.opentelemetry.proto.common.v1.KeyValue;
+import io.opentelemetry.proto.metrics.v1.AggregationTemporality;
+import io.opentelemetry.proto.metrics.v1.Metric;
+import io.opentelemetry.proto.metrics.v1.NumberDataPoint;
+import java.time.Instant;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 /**
  * This class represents a metric that does not yet contain resource tags.
  * These additional resource tags will be added before emitting metrics by the telemetry reporter.
@@ -23,9 +32,12 @@ package org.apache.kafka.common.telemetry.internals;
 public class SinglePointMetric implements MetricKeyable {
 
     private final MetricKey key;
+    private final Metric.Builder metricBuilder;
 
-    private SinglePointMetric(MetricKey key) {
-        this.key = key;
+    private SinglePointMetric(MetricKey key,
+        Metric.Builder metricBuilder) {
+      this.key = key;
+      this.metricBuilder = metricBuilder;
     }
 
     @Override
@@ -34,4 +46,146 @@ public class SinglePointMetric implements MetricKeyable {
     }
 
     // TODO: Implement methods for serializing/deserializing metrics in required format.
+    public Metric.Builder metric() {
+      return metricBuilder;
+    }
+
+    public static SinglePointMetric create(MetricKey metricKey, Metric.Builder metric) {
+      return new SinglePointMetric(metricKey, metric);
+    }
+
+    private static NumberDataPoint.Builder point(Instant timestamp, Number value) {
+      if (value instanceof Long || value instanceof Integer) {
+        return point(timestamp, value.longValue());
+      } else {
+        return point(timestamp, value.doubleValue());
+      }
+    }
+
+    private static NumberDataPoint.Builder point(Instant timestamp, long value) {
+      return NumberDataPoint.newBuilder()
+          .setTimeUnixNano(toTimeUnixNanos(timestamp))
+          .setAsInt(value);
+    }
+
+    private static NumberDataPoint.Builder point(Instant timestamp, double value) {
+      return NumberDataPoint.newBuilder()
+          .setTimeUnixNano(toTimeUnixNanos(timestamp))
+          .setAsDouble(value);
+    }
+
+    public static Iterable<KeyValue> asAttributes(Map<String, String> labels) {
+      return labels.entrySet().stream().map(
+          entry -> KeyValue.newBuilder()
+              .setKey(entry.getKey())
+              .setValue(AnyValue.newBuilder().setStringValue(entry.getValue())).build()
+      )::iterator;
+    }
+
+    /**
+     * creates a metric data point for arbitrary Number values.
+     * Since OpenTelemetry only supports double and long primitive types, the following mapping applies
+     * <p>
+     * Long / Integer -> long
+     * All other Number types -> double
+     * <p>
+     * For fine-grained control, use {@link #point(Instant, long)} and {@link #point(Instant, double)}
+     *
+     * @param metricKey
+     * @param value
+     * @param timestamp
+     * @return
+     */
+    public static SinglePointMetric gauge(MetricKey metricKey,
+        Number value,
+        Instant timestamp) {
+      NumberDataPoint.Builder point = point(timestamp, value);
+      return gauge(metricKey, point);
+    }
+
+    public static SinglePointMetric gauge(MetricKey metricKey,
+        long value,
+        Instant timestamp) {
+      NumberDataPoint.Builder point = point(timestamp, value);
+      return gauge(metricKey, point);
+    }
+
+    public static SinglePointMetric gauge(MetricKey metricKey,
+        double value,
+        Instant timestamp) {
+      NumberDataPoint.Builder point = point(timestamp, value);
+      return gauge(metricKey, point);
+    }
+
+    private static SinglePointMetric gauge(MetricKey metricKey, NumberDataPoint.Builder point) {
+      point.addAllAttributes(asAttributes(metricKey.tags()));
+
+      Metric.Builder metric = Metric.newBuilder()
+          .setName(metricKey.getName());
+
+      metric.getGaugeBuilder()
+          .addDataPoints(point);
+      return create(metricKey, metric);
+    }
+
+    public static SinglePointMetric deltaSum(MetricKey metricKey,
+        double value,
+        Instant timestamp,
+        Instant startTimestamp) {
+      NumberDataPoint.Builder point = point(timestamp, value)
+          .setStartTimeUnixNano(toTimeUnixNanos(startTimestamp));
+
+      return sum(metricKey, AggregationTemporality.AGGREGATION_TEMPORALITY_DELTA, point);
+    }
+
+
+    public static SinglePointMetric sum(MetricKey metricKey, long value, Instant timestamp) {
+      return sum(metricKey, value, timestamp, null);
+    }
+
+
+    public static SinglePointMetric sum(MetricKey metricKey, double value, Instant timestamp) {
+      return sum(metricKey, value, timestamp, null);
+    }
+
+    public static SinglePointMetric sum(MetricKey metricKey,
+        long value,
+        Instant timestamp,
+        Instant startTimestamp) {
+      NumberDataPoint.Builder point = point(timestamp, value);
+      if (startTimestamp != null) {
+        point.setStartTimeUnixNano(toTimeUnixNanos(startTimestamp));
+      }
+
+      return sum(metricKey, AggregationTemporality.AGGREGATION_TEMPORALITY_CUMULATIVE, point);
+    }
+
+    public static SinglePointMetric sum(MetricKey metricKey,
+        double value,
+        Instant timestamp,
+        Instant startTimestamp) {
+      NumberDataPoint.Builder point = point(timestamp, value);
+      if (startTimestamp != null) {
+        point.setStartTimeUnixNano(toTimeUnixNanos(startTimestamp));
+      }
+
+      return sum(metricKey, AggregationTemporality.AGGREGATION_TEMPORALITY_CUMULATIVE, point);
+    }
+
+    private static SinglePointMetric sum(MetricKey metricKey,
+        AggregationTemporality aggregationTemporality,
+        NumberDataPoint.Builder point) {
+      point.addAllAttributes(asAttributes(metricKey.tags()));
+      Metric.Builder metric = Metric.newBuilder().setName(metricKey.getName());
+
+      metric
+          .getSumBuilder()
+          .setAggregationTemporality(aggregationTemporality)
+          .addDataPoints(point);
+      return create(metricKey, metric);
+    }
+
+    private static long toTimeUnixNanos(Instant t) {
+      return TimeUnit.SECONDS.toNanos(t.getEpochSecond()) + t.getNano();
+    }
 }
